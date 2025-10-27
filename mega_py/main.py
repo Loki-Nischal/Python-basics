1dbfd34495e715e7f90412039984aecb4f09ffa1
Loki-Nischal <budhathoki.nischal@gmail.com>
2025-10-27 21:31:15 +0545

    last commit

---
+++ mega_py/main.py
@@ -0,0 +1,38 @@
+todos = []
+
+while True:
+    user_action = input("type add,show,edit,complete or exit: ")
+    user_action = user_action.strip() #it (strip) remove any whitespace from the user input
+
+    match user_action:
+        case "add":
+            todo = input("Enter a todo: ") + "\n"
+            todos.append(todo)
+
+        case "show":
+         
+            for index,item in enumerate(todos): #enumerate function returns both the index and the item from the list
+                print( f"{index+1}.{item}")
+
+        case "complete":
+            number = int(input("Number of the todo to complete: "))
+            todos.pop(number - 1)  #subtracting 1 because list index starts at 0
+                
+
+
+        case "edit":
+            number = int(input("Number of the todo to edit: "))
+            existing_todo = todos[number - 1]
+            print(f"You are editing: {existing_todo}")
+            new_todo = input("Enter the new todo: ") + "\n"
+            todos[number - 1] = new_todo 
+      
+
+        case "exit":
+            break
+
+print ("Bye!")  
+
+
+#Note strings are immutable, meaning you cannot change them in place. Instead, you create a new string and assign it to the same variable name if needed.
+#But list are mutable, meaning you can change their content without changing their identity.
